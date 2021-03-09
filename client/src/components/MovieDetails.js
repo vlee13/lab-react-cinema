84d737fc41ecabd4616bd11c2d2ad5a669f0008b
vlee13 <vlee13@gmail.com>
2021-03-09 07:59:50 -0500

    working with hooks

--- client/src/components/MovieDetails.js
+++ client/src/components/MovieDetails.js
@@ -1,17 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function MovieDetails(props) {
   let [deets, setDeets] = useState({});
 
-  axios
-    .get(`http://localhost:5000/movies/${props.match.params.id}`)
-    .then((res) => {
-      console.log(res);
-      setDeets(res.data);
-    });
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/movies/${props.match.params.id}`)
+      .then((res) => {
+        console.log(res);
+        setDeets(res.data);
+      });
+  }, []);
 
-  return <div>{deets.title}</div>;
+  return (
+    <div>
+      <h1>{deets.title}</h1>
+      <img src={deets.image} alt="moviepic" />
+      <h3>Showtimes:</h3>
+      <ul>
+        {deets.showtimes?.map((eachtime) => {
+          return <li>{eachtime}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default MovieDetails;
